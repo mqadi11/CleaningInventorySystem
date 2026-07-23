f04b02a1218ab00c0e703ac7894ea5a3c989a195
mqadi00 <mqadimkhanyisi@gmail.com>
2026-07-23 17:42:47 +0200

    Add User model

---
+++ src/cleaninginventorysystem/model/User.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cleaninginventorysystem.model;
+
+public class User {
+
+    private int userID;
+    private String fullName;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    public User() {
+
+    }
+
+    public User(int userID, String fullName, String username,
+                String email, String password, String role) {
+
+        this.userID = userID;
+        this.fullName = fullName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
