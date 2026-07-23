12db72ab21d807902079f2df0216a5f20b0109e4
mqadi00 <mqadimkhanyisi@gmail.com>
2026-07-24 00:13:15 +0200

    Complete authentication module

--- src/cleaninginventorysystem/dao/UserDAO.java
+++ src/cleaninginventorysystem/dao/UserDAO.java
@@ -2,6 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/**
+ * Handles database operations related to users.
+ *
+ * @author Mkhanyisi Mqadi
+ */
 package cleaninginventorysystem.dao;
 
 import cleaninginventorysystem.db.DBConnection;
--- src/cleaninginventorysystem/db/DBConnection.java
+++ src/cleaninginventorysystem/db/DBConnection.java
@@ -2,6 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/**
+ * Creates and manages connections to the PostgreSQL database.
+ *
+ * @author Mkhanyisi Mqadi
+ */
 package cleaninginventorysystem.db;
 
 import java.io.IOException;
